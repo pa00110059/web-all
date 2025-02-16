e72bb0312b32ee927c8f036d9bd7b4d80b968bf3
pa00110059 <pa00110059@gmail.com>
2025-02-16 17:02:48 +0800

    add WebUtil and use JSON

---
+++ web-all/schedule-system/src/com/rick/schedule/common/Result.java
@@ -0,0 +1,73 @@
+package com.rick.schedule.common;
+
+//        {
+//        "code":"100/200/300/400/401 ...",      業務狀態碼   本次請求的業務是否成功?    如果失敗了是什麼原因? 不是回應報文中的回應碼
+//                            "message":"業務狀態碼的  補充說明/描述"
+//                                    "data":{}   本次回應的數據     成功/不成功  List<Schedule> ...
+//        ... ...
+//        }
+
+public class Result<T> {
+    // 返回码
+    private Integer code;
+    // 返回消息
+    private String message;
+    // 返回数据
+    private T data;
+    public Result(){}
+    // 返回数据
+    protected static <T> Result<T> build(T data) {
+        Result<T> result = new Result<T>();
+        if (data != null)
+            result.setData(data);
+        return result;
+    }
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = build(body);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+    public static <T> Result<T> build(T body, ResultCodeEnum resultCodeEnum) {
+        Result<T> result = build(body);
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(resultCodeEnum.getMessage());
+        return result;
+    }
+    /**
+     * 操作成功
+     * @param data  baseCategory1List
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> ok(T data){
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.SUCCESS);
+    }
+    public Result<T> message(String msg){
+        this.setMessage(msg);
+        return this;
+    }
+    public Result<T> code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+}
--- web-all/schedule-system/src/com/rick/schedule/controller/SysUserController.java
+++ web-all/schedule-system/src/com/rick/schedule/controller/SysUserController.java
@@ -1,18 +1,19 @@
 package com.rick.schedule.controller;
 
+import com.rick.schedule.common.Result;
+import com.rick.schedule.common.ResultCodeEnum;
 import com.rick.schedule.pojo.SysUser;
 import com.rick.schedule.service.SysUserService;
 import com.rick.schedule.service.impl.SysUserServiceImpl;
 import com.rick.schedule.util.MD5Util;
+import com.rick.schedule.util.WebUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-
 
 
 @WebServlet("/user/*")
@@ -22,7 +23,35 @@ public class SysUserController extends BaseController {
     private SysUserService userService = new SysUserServiceImpl();
 
     /**
+     * 註冊時，接收要註冊的用戶名，檢驗用戶名是否被占用的業務接口
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void checkUsernameUsed(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //接收用戶名
+        String username = req.getParameter("username");
+
+        // 調用服務層業務處理方法查詢該用戶名是否有對應的用戶
+        SysUser sysUser = userService.findByUsername(username);
+        // 如果有  回覆 已佔用
+        // 如果沒有 回覆  可用
+
+        Result result = Result.ok(null);
+
+
+        if (null != sysUser) {
+            result = Result.build(null, ResultCodeEnum.USERNAME_USED);
+        }
+        // 將result對象轉換為JSON串回應給客戶端
+        WebUtil.writeJson(resp, result);
+    }
+
+    /**
      * 接受用戶登錄請求，完成登錄的業務接口
+     *
      * @param req
      * @param resp
      * @throws ServletException
@@ -33,31 +62,30 @@ public class SysUserController extends BaseController {
         String username = req.getParameter("username");
         String userPwd = req.getParameter("userPwd");
         //2 調用服務層方法,根據用戶名查詢用戶信息
-       SysUser loginUser = userService.findByUsername(username);
-       if (null == loginUser) {
-           // 跳轉到用戶名有誤提示頁
-           resp.sendRedirect("/loginUsernameError.html");
-       }else if(! MD5Util.encrypt(userPwd).equals(loginUser.getUserPwd())){
-           //3 判斷密碼是否一致
-           //跳轉到密碼有誤提示頁
-                resp.sendRedirect("/loginUsernameError.html");
-        }else {
+        SysUser loginUser = userService.findByUsername(username);
+        if (null == loginUser) {
+            // 跳轉到用戶名有誤提示頁
+            resp.sendRedirect("/loginUsernameError.html");
+        } else if (!MD5Util.encrypt(userPwd).equals(loginUser.getUserPwd())) {
+            //3 判斷密碼是否一致
+            //跳轉到密碼有誤提示頁
+            resp.sendRedirect("/loginUsernameError.html");
+        } else {
             //登錄成功之後,將登錄的用戶信息放入session
-           HttpSession session = req.getSession();
-           session.setAttribute("sysUser", loginUser);
+            HttpSession session = req.getSession();
+            session.setAttribute("sysUser", loginUser);
 
 
-           //4 跳轉到首頁
-           resp.sendRedirect("/showSchedule.html");
-       }
-
-
+            //4 跳轉到首頁
+            resp.sendRedirect("/showSchedule.html");
+        }
 
 
     }
 
     /**
      * 接收用戶註冊請求的業務處理方法(接口  不是java的interface)
+     *
      * @param req
      * @param resp
      * @throws ServletException
@@ -69,18 +97,16 @@ public class SysUserController extends BaseController {
         String username = req.getParameter("username");
         String userPwd = req.getParameter("userPwd");
         //2 調用服務層方法，完成註冊功能
-            //將參數放入一個SYSUser對象中,在調用regist方法時傳入
+        //將參數放入一個SYSUser對象中,在調用regist方法時傳入
         SysUser sysUser = new SysUser(null, username, userPwd);
         int rows = userService.regist(sysUser);
 
         //3 根據註冊結果(成功  失敗)做頁面跳轉
         if (rows > 0) {
             resp.sendRedirect("/registSuccess.html");
-        }else{
+        } else {
             resp.sendRedirect("/registFail.html");
         }
-
-
 
 
     }
